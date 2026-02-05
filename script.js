dbcbc1d0bbad0dd8afd4bd778f55f55ad4305501
Soumi Pal <soumiisc2020@gmail.com>
2026-02-05 08:18:46 +0530

    ui done

---
+++ script.js
@@ -0,0 +1,82 @@
+const filters = {
+    brightness: {
+        value: 100,
+        min: 0,
+        max: 200
+    },
+    contrast: {
+        value: 100,
+        min: 0,
+        max: 200
+    },
+    exposure: {
+        value: 0,
+        min: -100,
+        max: 100
+    },
+    saturation: {
+        value: 100,
+        min: 0,
+        max: 200
+    },
+    hueRotate: {
+        value: 0,
+        min: 0,
+        max: 360,
+        unit: "deg"
+    },
+    blur: {
+        value: 0,
+        min: 0,
+        max: 20,
+        unit: "px"
+    },
+    grayscale: {
+        value: 0,
+        min: 0,
+        max: 100,
+        unit: "%"
+    },
+    sepia: {
+        value: 0,
+        min: 0,
+        max: 100,
+        unit: "%"
+    },
+    opacity: {
+        value: 100,
+        min: 0,
+        max: 100,
+        unit: "%"
+    },
+    invert: {
+        value: 0,
+        min: 0,
+        max: 100,
+        unit: "%"
+    },
+}
+
+function createFilterElement(name, unit="%", value, min, max){
+    const filterContainer = document.createElement("div")
+    filterContainer.classList.add("filter")
+
+    const input = document.createElement("input")
+    input.type = "range"
+    input.min = min
+    input.max = max
+    input.value = value
+    input.id = name
+
+    const p = document.createElement("p")
+    p.innerText = name
+
+    dispatchEvent.appendChild(p)
+    dispatchEvent.appendChild(input)
+
+    return filterContainer
+}
+
+Object.keys(filters).forEach(key => {
+    console.log(key, filters[key]);
+})
